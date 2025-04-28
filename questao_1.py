80bc1b3e7db7d509e80ee0c7d2c3b446687f00c8
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-04-28 13:18:40 -0300

    Feat: uso do insertion sort e funcionalide de adicao de notas

--- questao_1.py
+++ questao_1.py
@@ -1,16 +1,51 @@
-def shell_sort(list:list[int]):
-    list_size = len(list)
-    gap = list_size // 2
-    while gap > 0 :
-        for i in range(0, list_size-gap):
-            aux = list[i]
-            if list[i + gap] < aux:
-                list[i] = list[i + gap]
-                list[i + gap] = aux 
-        gap-=1
+# Inserção Ordenada
+def insertion(list: list[float], index: int) -> list[float]:
+    # NOTE: Modifiquei o for para ele percorrer do index até 0
+    for item in range(index, 0, -1):
+        if list[item] > list[item-1]: # Verifica se o item atual é menor que o item que está a sua frente
+            aux = list[item] # Variável auxiliar que armazena o item atual
+            list[item] = list[item-1] # O item atual é trocado pelo item a sua frente
+            list[item-1] = aux # O item atual, que está armazenado na variável auxiliar, percorre uma posição para frente
+
+    # Retorna a lista ordenada
     return list
 
-lista = [6.7, 8.5, 5.4, 9.0, 7.8]
-print(shell_sort(lista))
-                
+# Insertion Sort
+def insertion_sort(list: list[float]) -> list[float]:
+    index = 1 # Variável responsável por armazenar a quantidade de passos do laço
+    while (index < len(list)): # Condição de parada
+        list = insertion(list, index) # A lista irá receber uma nova lista até que ela esteja totalmente ordenada
+        index += 1 # Acrescenta mais 1 a cada passo que o laço fizer
 
+    # Retorna a lista ordenada, após a condição do laço não ser mais verdadeira
+    return list
+
+# Lista que irá armazenar as notas finais dos alunos
+notas_finais = []
+
+while True:
+    # Menu de opções
+    opcao = int(input('[1] Adicionar nota\n[0] Finalizar\n-> '))
+
+    # Verifica se o usuário informou o número 0, para finalizar o laço
+    if opcao == 0:
+        # Verifica se o tamanho da lista é maior que 0
+        if len(notas_finais) > 0:
+            # Exibe a lista com a sequência informada pelo usuário
+            print(f'Entrada: {notas_finais}')
+            # Exibe a lista ordenada utilizando o insertion sort
+            print(f'Saída: {insertion_sort(notas_finais)}')
+        else:
+            print('Você não informou nenhum valor.')
+
+        break
+    # Verifica se o usuário informou o número 1, para adicionar uma nova nota
+    elif opcao == 1:
+        # Solicita que o usuário informe a nota
+        nota = float(input('Adicione a nota: '))
+        # Adiciona a nota na lista de notas finais
+        notas_finais.append(nota)
+
+    # Mensagem que será exibida, caso o usuário informe uma opção inválida do menu
+    else:
+        print('Opção inválida')
